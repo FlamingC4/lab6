569d55b5cd92b9fb2a38cbb4d725cf278ef42bea
Aaron Wong <flamingc4@gmail.com>
2018-02-15 02:56:23 -0800

    almost finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 function initializePage() {
 	$('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+	//$('#colorBtn').click(randomizeColors);
 }
 
 /*
@@ -20,11 +20,37 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
+	//document.getElementsByClassName("project").addEventListener("click", e.preventDefault());
+	$(".project").click(function(){
+		e.preventDefault();
+	});
+
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	//console.log(idNumber);
+	var url = "http://localhost:3000/project/" + idNumber;
+	var response = ""
+	$.get(url, response);
+	
+	var info = addProject(url);
+	console.log($.get(url, response));
+	//console.log("User clicked on project " + idNumber);
+	$(this).closest(".project").click(function(){		
+		console.log(this.id);
+		$(".thumbnail .details").html("hi");
+	});
+	
+}
 
-	console.log("User clicked on project " + idNumber);
+
+
+function addProject(result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
 }
